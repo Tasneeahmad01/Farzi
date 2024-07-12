b99aaa27d7fe067dfb41b319b5034953fefaf1fb
Your Name <you@example.com>
2024-07-12 16:04:06 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ComA from './ComA';
 import Num from './Num';
 import State from './Component/StateWiseData/State';
 import StateWise from './Component/StateWiseData/StateWise';
+import ComX from "./Component/StateWiseData/Compiler/ComX"
 
 const FirstName = createContext();
 
@@ -15,6 +16,8 @@ const App = () => {
         <Num />
         <State />
         <StateWise />
+        <br />
+        <ComX/>
       </FirstName.Provider>
     </>
   );
